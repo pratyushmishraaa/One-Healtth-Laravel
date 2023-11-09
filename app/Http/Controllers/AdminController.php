0c61e376fe5bcbb04eb37a3cf2a9a5fbf91bc9a6
Pratyush Mishra <pratyushmishra287@gmail.com>
2023-11-09 23:16:20 +0530

    second commit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,13 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Doctor;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\Appointment;
 
 class AdminController extends Controller
 {
     public function addview()
     {
-        return view('admin.add_doctor');
+        if(Auth::id())
+        {
+            if(Auth::user()->usertype==1)
+            {
+                return view('admin.add_doctor');
+            }
+            return redirect()->back();
+        }
+
+    else
+    {
+        return redirect('login');
     }
+
+}
     public function upload(Request $request)
     {
        $doctor = new doctor ;
@@ -35,10 +51,26 @@ class AdminController extends Controller
        $doctor->save();
 
        return redirect()->back()->with('message','Doctor Added Successfully');
+    }
+    public function showappointment()
+    {
+        $data=appointment::all();
+    return view('admin.showappointment',compact('data'));
+    }
+    public function approved($id)
+    {
+    $data=appointment::find($id);
+    $data->status='approved';
+    $data->save();
+    return redirect()->back()->with('message','Approved Successfully');
+    }
 
-
-
-
+    public function cancelled($id)
+    {
+    $data=appointment::find($id);
+    $data->status='Cancelled';
+    $data->save();
+    return redirect()->back()->with('message','Cancelled Successfully');
     }
 
 }
